c95e858ad89d6fe74d42569e4777931fd07e4880
Afolabi Adeniyi <aadeniyi88@gmail.com>
2020-01-15 21:51:30 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-
+def say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -10,4 +10,7 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+puts phrase
+end
+
 puts phrase
