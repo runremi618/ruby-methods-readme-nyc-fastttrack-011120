42aad092d5c1031bceb0d659af247d720ad27daa
Afolabi Adeniyi <aadeniyi88@gmail.com>
2020-01-15 21:55:19 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,16 +1,7 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting
+puts = "Hello World!"
+
 end
 
-puts phrase
+
